a778cf4ebfbd718a866e8adb72ac00375fabe986
Ruan <75811206+R-uan@users.noreply.github.com>
2024-07-18 17:18:09 -0300

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-gcc -c -Wall -Werror -fPIC libcsv.c -o ./lib/libcsv.o && \
-    gcc -shared -o ./lib/libcsv.so ./lib/libcsv.o
-
-gcc -o ./out/main main.c -L./lib -lcsv
-
-export LD_LIBRARY_PATH=./lib
-
-./out/main
